a476e47feb9a9514017b5dc7a49fe3e5b3c1c41e
Ethan Bull-Vulpe <ebullvul@login02.expanse.sdsc.edu>
2025-07-07 03:10:28 -0700

    progress on tutorial 2

--- tutorials/2_Many-Body_Model_Build-Along/Tutorial_2_setup.sh
+++ tutorials/2_Many-Body_Model_Build-Along/Tutorial_2_setup.sh
@@ -1,5 +1,5 @@
 conda activate p310
-cd /home/ebullvul/software/development/MB-Fit_versions/QDMS_2025/
+cd /expanse/lustre/projects/csd888/ebullvul/software/development/MB-Fit_versions/QDMS_2025
 export MBFIT_HOME=$PWD
 source sourceme.sh
 cd -
