8b9c95c8838ef828aebe293bed7a1ce79ffcfb49
311Richa <rrashmi@ucsd.edu>
2025-07-16 07:49:28 -0700

    tutorial 3 final

--- tutorials/3_advanced_applications/tutorial_3.3/8-SFG/do_spectra.sh
+++ tutorials/3_advanced_applications/tutorial_3.3/8-SFG/do_spectra.sh
@@ -2,7 +2,7 @@
 
 # this script is to plot the spectra given the TCF
 # to run: ./do_spectra.sh z1 z2 alpha
-# you need POSITION_CMD, DIPMOL_CMD, and DIPIND_CMD files
+# you need dump.dipoles file
 
 # temp is used to get the prefactor in the intensity but is currently commented in the code
 # alpha is the smoothing factor for the spectra
@@ -18,7 +18,7 @@ alpha=$3
 
 # ./spectrum-sfg temp tau alpha max_int < corr_dip.dat > spectrum.dat
 
-/p/home/rrashmi/software/parallel_MB_SFG_dipole_polarizability_fitting-master/dipole_polarizability_fitting-master/src/sfg/spectrum-sfg $temp 0 $alpha $int < response_1b_xxz_${z1}_${z2}.dat > spec_1b_xxz_${z1}_${z2}_a$alpha.dat
-/p/home/rrashmi/software/parallel_MB_SFG_dipole_polarizability_fitting-master/dipole_polarizability_fitting-master/src/sfg/spectrum-sfg $temp 0 $alpha $int < response_1bNb_xxz_${z1}_${z2}.dat > spec_1bNb_xxz_${z1}_${z2}_a$alpha.dat
-/p/home/rrashmi/software/parallel_MB_SFG_dipole_polarizability_fitting-master/dipole_polarizability_fitting-master/src/sfg/spectrum-sfg $temp 0 $alpha $int < response_1b2bNb_xxz_${z1}_${z2}.dat > spec_1b2bNb_xxz_${z1}_${z2}_a$alpha.dat
+/home/rrashmi/software/mb-spec/src/sfg/spectrum-sfg $temp 0 $alpha $int < response_1b_xxz_${z1}_${z2}.dat > spec_1b_xxz_${z1}_${z2}_a$alpha.dat
+/home/rrashmi/software/mb-spec/src/sfg/spectrum-sfg $temp 0 $alpha $int < response_1bNb_xxz_${z1}_${z2}.dat > spec_1bNb_xxz_${z1}_${z2}_a$alpha.dat
+/home/rrashmi/software/mb-spec/src/sfg/spectrum-sfg $temp 0 $alpha $int < response_1b2bNb_xxz_${z1}_${z2}.dat > spec_1b2bNb_xxz_${z1}_${z2}_a$alpha.dat
 
